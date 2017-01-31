73f4c8956f560dbb1af2091d327e16bc4b17591a
Windy Wang <windy.znw@gmail.com>
2017-01-31 16:22:36 -0500

    Included uVDBP distribution with and without log function

--- R/graphs.R
+++ R/graphs.R
@@ -1,7 +1,11 @@
+
 # Boxplot -------------------------------
+
 box_plot <- function(data, xvar, yvar, xlab="", ylab="") {
   ggplot2::ggplot(data, ggplot2::aes_string(x = xvar, y = yvar)) +
-    ggplot2::geom_boxplot(ggplot2::aes_string(colour = xvar, fill = xvar)) +
+    ggplot2::geom_jitter(ggplot2::aes_string(colour = xvar, alpha = 0.4), width = 0.33) +
+    ggplot2::geom_boxplot(ggplot2::aes_string(colour = xvar, fill = xvar), 
+                          outlier.shape = NA) +
     ggplot2::stat_summary(geom = "crossbar", width = 0.65, fatten = 0, color = "white",
                           fun.data = function(x){
                             return(c(y = median(x), ymin = median(x), ymax = median(x)))
@@ -18,16 +22,34 @@ box_plot <- function(data, xvar, yvar, xlab="", ylab="") {
     ggplot2::ylab(ylab)
 }
 
-# scale_x_discrete(labels = paste(levels(ds_base$xvar), 
-#                                 "\n(N=", table(ds_base$xvar), ")", sep = "")) +
+# Scatterplot -------------------------------------------
 
+scatter_plot = function(data, xvar, yvar, xlab='', ylab='') {
+  ggplot(data, aes_string(x=xvar, y=yvar)) +
+    geom_point(colour = "#0db7c4", size = 1) + #mapping=aes(color=mcr_status)
+    theme_minimal() + 
+    theme(panel.grid.major.x = element_blank(), 
+          axis.line.y = element_blank(),
+          axis.text.y = element_text(colour = "grey"),
+          axis.ticks.y = element_line(colour = "grey"),
+          axis.text.x = element_text(colour = "grey50"), #angle = 45
+          axis.title = element_text(size = 10)) +
+    xlab(xlab) +
+    ylab(ylab)
+}
 
+# Histogram -----------------------------------------------
 
-# box_plot = function(data, xvar, yvar, xlab='', ylab='') {
-#   ggplot(data, aes(x=xvar, y=yvar)) +
-#     geom_boxplot() +
-#     # scale_x_discrete(limits=xcat) +
-#     xlab(xlab) +
-#     ylab(ylab) +
-#     theme_bw()
-# }
+histo_plot = function(data, variable, bin, xlab='') {
+  ggplot2::ggplot(data, ggplot2::aes_string(x=variable)) +
+    ggplot2::geom_histogram(binwidth=bin,
+                            colour='#0db7c4', fill='#0db7c4') +
+    ggplot2::xlab(xlab) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(), 
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(colour = "grey"),
+                   axis.ticks.y = ggplot2::element_line(colour = "grey"),
+                   axis.text.x = ggplot2::element_text(colour = "grey 30"), #angle = 45
+                   axis.title = ggplot2::element_text(size = 10)) 
+}

1b9959e43c9edd82a65a09756fd99f3495968ea4
Windy Wang <windy.znw@gmail.com>
2017-01-31 16:33:44 -0500

    Graph functions can be used without loading ggplot2 package

--- R/graphs.R
+++ R/graphs.R
@@ -25,17 +25,17 @@ box_plot <- function(data, xvar, yvar, xlab="", ylab="") {
 # Scatterplot -------------------------------------------
 
 scatter_plot = function(data, xvar, yvar, xlab='', ylab='') {
-  ggplot(data, aes_string(x=xvar, y=yvar)) +
-    geom_point(colour = "#0db7c4", size = 1) + #mapping=aes(color=mcr_status)
-    theme_minimal() + 
-    theme(panel.grid.major.x = element_blank(), 
-          axis.line.y = element_blank(),
-          axis.text.y = element_text(colour = "grey"),
-          axis.ticks.y = element_line(colour = "grey"),
-          axis.text.x = element_text(colour = "grey50"), #angle = 45
-          axis.title = element_text(size = 10)) +
-    xlab(xlab) +
-    ylab(ylab)
+  ggplot2::ggplot(data, ggplot2::aes_string(x=xvar, y=yvar)) +
+    ggplot2::geom_point(colour = "#0db7c4", size = 1) + #mapping=aes(color=mcr_status)
+    ggplot2::theme_minimal() + 
+    ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(), 
+          axis.line.y = ggplot2::element_blank(),
+          axis.text.y = ggplot2::element_text(colour = "grey"),
+          axis.ticks.y = ggplot2::element_line(colour = "grey"),
+          axis.text.x = ggplot2::element_text(colour = "grey50"), #angle = 45
+          axis.title = ggplot2::element_text(size = 10)) +
+    ggplot2::xlab(xlab) +
+    ggplot2::ylab(ylab)
 }
 
 # Histogram -----------------------------------------------
